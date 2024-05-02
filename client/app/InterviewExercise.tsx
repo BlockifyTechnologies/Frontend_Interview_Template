acbbd6d33c31161d1cd14266c2e7c3820b77f653
Khouz <marc@blockify.tech>
2024-05-02 17:53:36 +0300

    pushing

--- client/app/InterviewExercise.tsx
+++ client/app/InterviewExercise.tsx
@@ -1,34 +1,4 @@
 import React from 'react';
-const users = [
-  {
-    id: 1,
-    name: 'John',
-    gender: 'male',
-    details: { age: 22, address: '123 Maple St' },
-  },
-  {
-    id: 2,
-    name: 'Jane',
-    gender: 'female',
-    details: { age: 28, address: '234 Oak St' },
-  },
-  {
-    id: 3,
-    name: 'Joe',
-    gender: 'male',
-    details: { age: 32, address: '456 Cedar St' },
-  },
-];
-type details = {
-  age: number;
-  address: string;
-};
-type userType = {
-  id: number;
-  name: string;
-  gender: 'male' | 'female';
-  details: details;
-};
 
 const Page = () => {
   return <div>Page</div>;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,40 +1,4 @@
 import React from 'react';
-const users = [
-  {
-    id: 1,
-    name: 'John',
-    gender: 'male',
-    details: { age: 22, address: '123 Maple St' },
-  },
-  {
-    id: 2,
-    name: 'Jane',
-    gender: 'female',
-    details: { age: 28, address: '234 Oak St' },
-  },
-  {
-    id: 3,
-    name: 'Joe',
-    gender: 'male',
-    details: { age: 32, address: '456 Cedar St' },
-  },
-  {
-    id: 4,
-    name: 'Jad',
-    gender: 'male',
-    details: { age: 62, address: '789 Pine St' },
-  },
-];
-type details = {
-  age: number;
-  address: string;
-};
-type userType = {
-  id: number;
-  name: string;
-  gender: 'male' | 'female';
-  details: details;
-};
 
 const page = () => {
   return <div></div>;
